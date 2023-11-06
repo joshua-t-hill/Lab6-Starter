93baba4a7044a88802962b916b8f7866fa6b7e17
Mitchell Bricco <briccm84@uwosh.edu>
2023-11-06 09:06:16 -0600

    added methods maxDist and StartingAirport, and Properties to RoutingStrategies

--- RoutingStrategies.xaml.cs
+++ RoutingStrategies.xaml.cs
@@ -1,11 +1,26 @@
-﻿namespace Lab6_Starter;
+﻿using Lab6_Starter.Model;
+
+namespace Lab6_Starter;
 
 public partial class RoutingStrategies : ContentPage
 {
-
+    public int MaxDist {get; set;}
+    public bool Visited { get; set;}
+    public Airport StartingAirport { get; set;}
     public RoutingStrategies()
     {
         InitializeComponent();
+        //BindingContext = MauiProgram.BusinessLogic;
+    }
+
+    void MaxDist_OnEntryCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
+    }
+
+    void StartingAirport_OnEntryCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
     }
 
 }
