783c042e49aca92c9154702dd78e45e82b63babb
briccm84 <msbricco@gmail.com>
2023-11-18 17:20:01 -0600

    added MeteorologistStatic, a simplified, static, and methods return formatted strings ready to be displayed on the weatherPage

---
+++ Model/MeteorologistStatic.cs
@@ -0,0 +1,96 @@
+﻿using RestSharp;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Lab2_Solution.Model;
+/// <summary>
+/// Implementation of API for Lab7P2 (Lab8) done by Group 5.
+/// BusinessLogic layer file for Weather page.
+/// </summary>
+public static class MeteorologistStatic
+{
+    private static readonly string keyAPI = "53b1c7659260454e889e86c373"; //our api key for accessing https://www.checkwxapi.com
+
+    /// <summary>
+    /// gets Metar and formats it for the weatherPage
+    /// </summary>
+    public static string GetMetar(string airportId)
+    {
+        string metarClientString = "https://api.checkwx.com/metar/" + airportId + "/nearest";
+        RestClient client = new(string.Format(metarClientString, airportId));
+        RestRequest request = new()
+        {
+            Method = Method.Get
+        };
+
+        request.AddHeader("X-API-Key", keyAPI);
+        RestResponse response = client.Execute(request);
+        string jsonResponse = response.Content;
+        try
+        {
+            using (JsonDocument document = JsonDocument.Parse(jsonResponse))
+            {
+                JsonElement root = document.RootElement;
+
+                // Check if the structure is as expected
+                if (root.TryGetProperty("data", out JsonElement dataElement) && dataElement.GetArrayLength() > 0)
+                {
+                    // Get the first item in the 'data' array
+                    JsonElement firstDataElement = dataElement.EnumerateArray().First();
+
+                    // Extract the string value
+                    return firstDataElement.GetString();
+                }
+            }
+
+            Console.WriteLine("Unexpected JSON structure");
+            return null;
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine($"Error parsing JSON: {ex.Message}");
+            return null;
+        }
+    }
+    /// <summary>
+    /// gets taf and formats it for the weatherPage
+    /// </summary>
+    public static string GetTaf(string airportId)
+    {
+        string tafClientString = "https://api.checkwx.com/taf/" + airportId + "/nearest";
+        RestClient client = new(string.Format(tafClientString, airportId));
+        RestRequest request = new()
+        {
+            Method = Method.Get
+        };
+
+        request.AddHeader("X-API-Key", keyAPI);
+        RestResponse response = client.Execute(request);
+        string jsonResponse = response.Content;
+        try
+        {
+            using (JsonDocument document = JsonDocument.Parse(jsonResponse))
+            {
+                JsonElement root = document.RootElement;
+
+                // Check if the structure is as expected
+                if (root.TryGetProperty("data", out JsonElement dataElement) && dataElement.GetArrayLength() > 0)
+                {
+                    // Get the first item in the 'data' array
+                    JsonElement firstDataElement = dataElement.EnumerateArray().First();
+
+                    // Extract the string value
+                    return firstDataElement.GetString();
+                }
+            }
+
+            Console.WriteLine("Unexpected JSON structure");
+            return null;
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine($"Error parsing JSON: {ex.Message}");
+            return null;
+        }
+    }
+}
